93c94ae2a445ceb065ab9d9e056c815ce7d47481
Mingmanas Sivaraksa <62439174+AjMing@users.noreply.github.com>
2021-11-26 04:16:21 +0000

    add all constructor

--- main.cpp
+++ main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main() {
    monster m("Venom",1000,100) ;
    monster x("Thanakit"); // hp=100 potion=10
+   monster *p;
 
-   monster y[10];
+   monster y[10]={{"Chaowalee",50,1},{"Pisit"},{"Chanchon"}};
+
+   p=new monster("Jaimai");
 } 
